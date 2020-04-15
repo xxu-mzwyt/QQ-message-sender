82e68e0660e7567437efaf2b8bb313e9c16d75b2
Benjamin Hsu <38581848+mzWyt@users.noreply.github.com>
2020-04-15 23:29:44 +0800

    Update qq message sender.py

--- qq message sender.py
+++ qq message sender.py
@@ -7,7 +7,11 @@ import win32con
 import win32clipboard as w
 import random
 
- 
+
+target_time = [649, 750]
+last_time = 0
+
+
 def send_message(name, msg):
 	w.OpenClipboard()
 	w.EmptyClipboard()
@@ -22,20 +26,19 @@ def send_message(name, msg):
 
 	win32gui.SendMessage(handle, win32con.WM_KEYDOWN, win32con.VK_RETURN, 0)
 
-target_time = [649, 750]
-last_time = 0
+def timer(gap):
+	while True:
+		req_time = time.localtime(time.time())
+		curr_time = req_time.tm_hour * 100 + req_time.tm_min 
+		print("Time checked. Current time: " + str(curr_time))
+		if curr_time in target_time and curr_time != last_time:
+			print("Time checked and it's set time now.")
+			send_message("我的iPhone", "test")
+			last_time = curr_time
+		time.sleep(gap)
 
-while True:
-	req_time = time.localtime(time.time())
-	curr_time = req_time.tm_hour * 100 + req_time.tm_min 
-	print("Time checked. Current time: " + str(curr_time))
-	if curr_time in target_time and curr_time != last_time:
-		print("Time checked and it's set time now.")
-		send_message("Speaking & Writing", "打卡")
-		# send_message("我的iPhone", "test")
-		last_time = curr_time
-	time.sleep(20)
-
+def MAIN():
+	timer(60)
 
 
 
